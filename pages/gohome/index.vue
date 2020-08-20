38479c42886950615157796624f06d2b68c66834
takashi0602 <takashioicsn@gmail.com>
2020-08-21 02:47:56 +0900

    add: rule gohome pages fix: top page

---
+++ pages/gohome/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h3>gohome</h3>
+    <nuxt-link to="/">おつかれ</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
---
+++ pages/rule/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h3>あそびかた</h3>
+    <div>
+      ここにルールが入ります。
+    </div>
+    <nuxt-link to="/">もどる</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>

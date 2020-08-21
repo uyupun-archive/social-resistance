c8976a3fd59433825404f23463ee6889b192410f
Kazuki Tobita <kazukiti201@gmail.com>
2020-08-22 02:49:55 +0900

    Modify pca

--- word2vec/pca.py
+++ word2vec/pca.py
@@ -7,6 +7,32 @@ from sklearn.decomposition import PCA
 import os
 import pathlib
 import json
+import re
+
+# ゴミデータの除去
+def isGarbageWord(word):
+    # 半角数字の除去
+    if re.compile('[0-9]+').fullmatch(word):
+        return True
+    # 全角数字の除去
+    if re.compile('[０-９]+').fullmatch(word):
+        return True
+    # ひらがなのみで構成される単語の除去
+    if re.compile('[\u3041-\u309F]+').fullmatch(word) and len(word):
+        return True
+    # 全角カタカナ１文字の除去
+    if re.compile('[\u30A1-\u30FF]+').fullmatch(word) and len(word):
+        return True
+    # 半角カタカナのみで構成される単語の除去
+    if re.compile('[\uFF66-\uFF9F]+').fullmatch(word):
+        return True
+    # 半角記号
+    if re.compile('[\u0020-\u002F\u003A-\u0040\u005B-\u0060\u007B-\u007E]+').match(word):
+        return True
+    # 全角記号
+    if re.compile('[\u3000-\u303F\uFF01-\uFF0F\uFF1A-\uFF20\uFF3B-\uFF40\uFF5B-\uFF65\uFF9E-\uFFEE]+').match(word):
+        return True
+    return False
 
 model = word2vec.Word2Vec.load('./model')
 words = model.wv.index2word
@@ -15,8 +41,9 @@ words = model.wv.index2word
 vectors =[]
 names = []
 for word in words:
-    vectors.append(model[word])
-    names.append(word)
+    if not isGarbageWord(word):
+        vectors.append(model[word])
+        names.append(word)
 
 # PCA
 pca = PCA(n_components=2)

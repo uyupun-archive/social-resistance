9460c9c9a82d57cdd2f1b7963223c7ae0d1a5d62
takashi0602 <takashioicsn@gmail.com>
2020-08-25 01:09:03 +0900

    fix: @font-face

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,8 +9,12 @@
 <style lang="scss">
 @font-face {
   font-family: 'Roboto-Thin';
-  src: url('~assets/fonts/Roboto-Thin.ttf') format('truetype'),
-    url('~assets/fonts/SawarabiGothic-Regular.ttf') format('truetype');
+  src: url('~assets/fonts/Roboto-Thin.ttf') format('truetype');
+}
+
+@font-face {
+  font-family: 'Sawarabi Gothic';
+  src: url('~assets/fonts/SawarabiGothic-Regular.ttf') format('truetype');
 }
 
 html {

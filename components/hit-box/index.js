d578680b967aa8b925addf8b332968f765335ed5
Kazuki Tobita <kazukiti201@gmail.com>
2020-08-26 04:56:28 +0900

    Add HitBox

---
+++ components/hit-box/index.js
@@ -0,0 +1,19 @@
+export default class HitBox {
+  /**
+   * 当たり判定
+   *
+   * @param {*} coordinateA
+   * @param {*} coordinateB
+   */
+  isHit(coordinateA, coordinateB) {
+    // 二点間の距離
+    const distance = Math.sqrt(
+      (coordinateB.x - coordinateA.x) ** 2 +
+        (coordinateB.y - coordinateA.y) ** 2
+    )
+    const radius = 60
+    // 二点間の距離が各Playerの和以下なら衝突している
+    if (distance <= radius * 2) return true
+    return false
+  }
+}

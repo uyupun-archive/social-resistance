347edeb1d730dfe006953c4213a8d952881e5fc4
Kazuki Tobita <kazukiti201@gmail.com>
2020-08-28 20:47:34 +0900

    Modify word2vec

--- plugins/word2vec.js
+++ plugins/word2vec.js
@@ -1,4 +1,5 @@
 import word2vec from '@/assets/json/word2vec.json'
+import { WORD_COUNT, WORD_DIRECTION } from '@/components/constants/index.js'
 
 /**
  * 最初の１単語を返す
@@ -17,11 +18,10 @@ const getFirstWord = () => {
  */
 const getWords = (baseWord) => {
   const words = []
-  for (let i = 0; i < 8; i++) {
+  for (let i = 0; i < WORD_COUNT; i++) {
     const idx = _getRandomIdx()
     const word = word2vec[idx]
     word.index = idx
-    word.move.correct_x = _getCorrectX(baseWord, word)
     word.direction = _getDirection(baseWord, word)
     words.push(word)
   }
@@ -37,23 +37,6 @@ const _getRandomIdx = () => {
 }
 
 /**
- * ベースとなる単語のxの値よりも比較対象の単語のxの値が小さい場合、
- * 後退してしまうのでそのための措置
- * TODO: 猟犬サイドの場合は計算が逆転する。修正が必要
- *
- * @param {*} baseWord
- * @param {*} word
- */
-const _getCorrectX = (baseWord, word) => {
-  if (word.move.x < baseWord.move.x) {
-    const correctX =
-      baseWord.move.x + (Math.abs(word.move.x) - Math.abs(baseWord.move.x))
-    return correctX
-  }
-  return word.move.x
-}
-
-/**
  * 単語の選択によって現在の位置から大まかにどの方向へ移動するかのガイド
  * 四象限で表現
  *
@@ -62,19 +45,11 @@ const _getCorrectX = (baseWord, word) => {
  */
 const _getDirection = (baseWord, word) => {
   if (word.move.x > baseWord.move.x) {
-    if (word.move.y > baseWord.move.y) {
-      // 第四象限
-      return 'bottom-right'
-    }
-    // 第一象限
-    return 'top-right'
+    if (word.move.y > baseWord.move.y) return WORD_DIRECTION.BOTTOM_RIGHT
+    return WORD_DIRECTION.TOP_RIGHT
   }
-  if (word.move.y > baseWord.move.y) {
-    // 第二象限
-    return 'bottom-left'
-  }
-  // 第三象限
-  return 'top-left'
+  if (word.move.y > baseWord.move.y) return WORD_DIRECTION.BOTTOM_LEFT
+  return WORD_DIRECTION.TOP_LEFT
 }
 
 /* eslint-disable */

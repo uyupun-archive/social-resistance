b82a32794d0d159ef4ed7d20a12acfe315089cfe
Kazuki Tobita <kazukiti201@gmail.com>
2020-08-29 14:30:31 +0900

    Delete garbage data

--- word2vec/pca.py
+++ word2vec/pca.py
@@ -3,47 +3,27 @@
 
 from gensim.models import word2vec
 from sklearn.decomposition import PCA
+from janome.tokenizer import Tokenizer
 
 import os
 import pathlib
 import json
 import re
 
-# ゴミデータの除去
-def isGarbageWord(word):
-    # 半角数字の除去
-    if re.compile('[0-9]+').fullmatch(word):
-        return True
-    # 全角数字の除去
-    if re.compile('[０-９]+').fullmatch(word):
-        return True
-    # ひらがなのみで構成される単語の除去
-    if re.compile('[\u3041-\u309F]+').fullmatch(word) and len(word):
-        return True
-    # 全角カタカナ１文字の除去
-    if re.compile('[\u30A1-\u30FF]+').fullmatch(word) and len(word):
-        return True
-    # 半角カタカナのみで構成される単語の除去
-    if re.compile('[\uFF66-\uFF9F]+').fullmatch(word):
-        return True
-    # 半角記号
-    if re.compile('[\u0020-\u002F\u003A-\u0040\u005B-\u0060\u007B-\u007E]+').match(word):
-        return True
-    # 全角記号
-    if re.compile('[\u3000-\u303F\uFF01-\uFF0F\uFF1A-\uFF20\uFF3B-\uFF40\uFF5B-\uFF65\uFF9E-\uFFEE]+').match(word):
-        return True
-    return False
-
 model = word2vec.Word2Vec.load('./model')
 words = model.wv.index2word
 
 # PCAするための前準備
+# JanomeのTokenizerによって品詞分解、名詞のみを抽出している
 vectors =[]
 names = []
+t = Tokenizer()
 for word in words:
-    if not isGarbageWord(word):
-        vectors.append(model[word])
-        names.append(word)
+    tokens = t.tokenize(word)
+    for token in tokens:
+        if (token.part_of_speech.split(',')[0] == '名詞'):
+            vectors.append(model[word])
+            names.append(word)
 
 # PCA
 pca = PCA(n_components=2)

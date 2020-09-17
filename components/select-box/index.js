66a49745654ec80c4e1cc4f304d6c9a82ab7ab12
takashi0602 <takashioicsn@gmail.com>
2020-09-17 20:40:54 +0900

    modify: SelectBox component, add: uuid

--- components/select-box/index.js
+++ components/select-box/index.js
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from 'uuid'
+
 export default {
   props: {
     options: {
@@ -18,12 +20,77 @@ export default {
   },
   data() {
     return {
-      selectModel: this.selected,
+      selectedValue: this.selected,
+      selectedData: null,
+      showList: false,
+      isCloseList: false,
+      selectId: uuidv4(),
     }
+  },
+  mounted() {
+    document.addEventListener('click', this.clickOutSide, false)
+    this.onSelect()
+  },
+  destroyed() {
+    document.removeEventListener(('click', this.clickOutSide, false))
   },
   methods: {
     getSelected() {
-      return this.selectModel
+      return this.selectedValue
+    },
+    onSelect(value = this.selectedValue) {
+      this.selectedData = this.options.filter(
+        (option) => option.value === value
+      )[0]
+      this.selectedValue = this.selectedData.value
+    },
+    onClickSelectBox() {
+      if (this.showList) {
+        this.closeList()
+      } else {
+        this.showList = true
+      }
+    },
+    onCheckSelected(value) {
+      return this.selectedValue === value
+    },
+    onSelectItem(value) {
+      if (this.selectedValue !== value) {
+        this.onSelect(value)
+        this.closeList()
+      }
+    },
+    closeList() {
+      this.isCloseList = true
+      setTimeout(() => {
+        this.showList = false
+        this.isCloseList = false
+      }, 300)
+    },
+    clickOutSide(element) {
+      if (!this.showList) return
+
+      const selectComponentElement = document.getElementById(this.selectId)
+      const divElement = selectComponentElement.lastElementChild
+      if (divElement.children.length <= 1) return
+
+      const selectBoxElement = divElement.firstElementChild
+      const selectBoxIconElement = selectBoxElement.firstElementChild
+      if (
+        selectBoxElement === element.target ||
+        selectBoxIconElement === element.target
+      ) {
+        return
+      }
+
+      const listElement = divElement.lastElementChild
+      if (listElement === element.target) return
+
+      const itemElements = listElement.children
+      const foundIndex = Array.from(itemElements).indexOf(element.target)
+      if (foundIndex === -1) {
+        this.closeList()
+      }
     },
   },
 }

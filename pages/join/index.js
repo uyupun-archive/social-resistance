085adc2d9b6301eec8129a4537ca50268af2de9a
suzuking1125 <b7165@oic.jp>
2020-09-19 00:30:14 +0900

    joinフォームのテキストボックスをコンポーネント化しました

--- pages/join/index.js
+++ pages/join/index.js
@@ -1,12 +1,9 @@
 import Button from '~/components/button/index.vue'
+import Textbox from '~/components/textbox/index.vue'
 
 export default {
   components: {
     Button,
-  },
-  data() {
-    return {
-      worldid: '',
-    }
+    Textbox,
   },
 }

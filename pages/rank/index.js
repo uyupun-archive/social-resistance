409d1bfa3f8c74508733033ea86b90b2bd4259d2
suzuking1125 <b7165@oic.jp>
2020-10-23 01:32:32 +0900

    rename listname , fix rate

--- pages/rank/index.js
+++ pages/rank/index.js
@@ -1,17 +1,17 @@
 export default {
   data() {
     return {
-      ranklist: [
-        { name: 'Pandemic', rate: '～青天井' },
-        { name: 'Cluster', rate: '～1799' },
-        { name: 'Aerosol', rate: '～1599' },
-        { name: 'Virus', rate: '～1399' },
-        { name: 'Vaccine', rate: '～1199' },
-        { name: 'Doctor', rate: '～999' },
-        { name: 'Mask', rate: '～799' },
-        { name: 'Resistance', rate: '～599' },
-        { name: 'Patient', rate: '～399' },
-        { name: 'Zombie', rate: '～199' },
+      ranks: [
+        { name: 'Pandemic', rate: '1800～' },
+        { name: 'Cluster', rate: '1600～1799' },
+        { name: 'Aerosol', rate: '1400～1599' },
+        { name: 'Virus', rate: '1200～1399' },
+        { name: 'Vaccine', rate: '1000～1199' },
+        { name: 'Doctor', rate: '800～999' },
+        { name: 'Mask', rate: '600～799' },
+        { name: 'Resistance', rate: '400～599' },
+        { name: 'Patient', rate: '200～399' },
+        { name: 'Zombie', rate: '0～199' },
       ],
     }
   },

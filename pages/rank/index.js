353e87f46faa0ecfb6ed1c15f1ae87580339e271
suzuking1125 <b7165@oic.jp>
2020-10-23 01:11:27 +0900

    add ranklist date

---
+++ pages/rank/index.js
@@ -0,0 +1,18 @@
+export default {
+  data() {
+    return {
+      ranklist: [
+        { name: 'Pandemic', rate: '～青天井' },
+        { name: 'Cluster', rate: '～1799' },
+        { name: 'Aerosol', rate: '～1599' },
+        { name: 'Virus', rate: '～1399' },
+        { name: 'Vaccine', rate: '～1199' },
+        { name: 'Doctor', rate: '～999' },
+        { name: 'Mask', rate: '～799' },
+        { name: 'Resistance', rate: '～599' },
+        { name: 'Patient', rate: '～399' },
+        { name: 'Zombie', rate: '～199' },
+      ],
+    }
+  },
+}

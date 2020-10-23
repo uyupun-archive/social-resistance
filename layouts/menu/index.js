beac594d0d31e2f8aa8c8b4e3c62b0b75e18c0ed
takashi0602 <takashioicsn@gmail.com>
2020-10-23 16:33:08 +0900

    add: menu layout. modify: sessionStorage.token to top, login, register

---
+++ layouts/menu/index.js
@@ -0,0 +1,7 @@
+import Menu from '~/components/menu/index.vue'
+
+export default {
+  components: {
+    Menu,
+  },
+}
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ export default {
   components: {
     Button,
   },
+  layout() {
+    return sessionStorage.token ? 'menu/index' : 'default'
+  },
   data() {
     return {
       logo: `${process.env.MITSU_URL}/images/logo/logo_white.svg`,

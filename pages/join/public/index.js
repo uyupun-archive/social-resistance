b68f23bc02c46638aeb0f5495fbb35c0f5660960
murakami1618 <b7350@oic.jp>
2020-10-28 22:02:51 +0900

    create public page

---
+++ pages/join/public/index.js
@@ -0,0 +1,20 @@
+import Button from '~/components/button/index.vue'
+
+export default {
+  components: {
+    Button,
+  },
+  data() {
+    return {
+      rooms: [
+        { worldId: 'ABCDEF', charcter: 'ばいきんくん' },
+        { worldId: 'ABCDEF', charcter: 'ばいきんくん' },
+        { worldId: 'ABCDEF', charcter: 'ばいきんくん' },
+        { worldId: 'ABCDEF', charcter: 'ばいきんくん' },
+        { worldId: 'ABCDEF', charcter: 'ばいきんくん' },
+        { worldId: 'ABCDEF', charcter: 'ばいきんくん' },
+        { worldId: 'ABCDEF', charcter: 'ばいきんくん' },
+      ],
+    }
+  },
+}

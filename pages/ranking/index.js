9024d6ad9a91e9eac9dd3b161c697e4216ee9bd9
murakami1618 <b7350@oic.jp>
2020-10-29 15:46:36 +0900

    create ranking mock

---
+++ pages/ranking/index.js
@@ -0,0 +1,20 @@
+import Button from '~/components/button/index.vue'
+
+export default {
+  components: {
+    Button,
+  },
+  data() {
+    return {
+      users: [
+        { name: 'ばいきんくん', score: '9999', rank: '1st' },
+        { name: 'ばいきんくん', score: '9999', rank: '2nd' },
+        { name: 'ばいきんくん', score: '9999', rank: '3rd' },
+        { name: 'ばいきんくん', score: '9999', rank: '4th' },
+        { name: 'ばいきんくん', score: '9999', rank: '5th' },
+        { name: 'ばいきんくん', score: '9999', rank: '6th' },
+        { name: 'ばいきんくん', score: '9999', rank: '7th' },
+      ],
+    }
+  },
+}

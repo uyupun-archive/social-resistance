3c363a7aa69fd19efa00050a93962586bf1a4490
murakami1618 <b7350@oic.jp>
2020-10-30 16:51:48 +0900

    review

--- pages/join/public/index.js
+++ pages/join/public/index.js
@@ -6,14 +6,14 @@ export default {
   },
   data() {
     return {
-      rooms: [
-        { worldId: 'ABCDEF', charcter: 'ばいきんくん' },
-        { worldId: 'ABCDEF', charcter: 'ばいきんくん' },
-        { worldId: 'ABCDEF', charcter: 'ばいきんくん' },
-        { worldId: 'ABCDEF', charcter: 'ばいきんくん' },
-        { worldId: 'ABCDEF', charcter: 'ばいきんくん' },
-        { worldId: 'ABCDEF', charcter: 'ばいきんくん' },
-        { worldId: 'ABCDEF', charcter: 'ばいきんくん' },
+      worlds: [
+        { Id: 'ABCDEF', charcter: 'ばいきんくん' },
+        { Id: 'ABCDEF', charcter: 'ばいきんくん' },
+        { Id: 'ABCDEF', charcter: 'ばいきんくん' },
+        { Id: 'ABCDEF', charcter: 'ばいきんくん' },
+        { Id: 'ABCDEF', charcter: 'ばいきんくん' },
+        { Id: 'ABCDEF', charcter: 'ばいきんくん' },
+        { Id: 'ABCDEF', charcter: 'ばいきんくん' },
       ],
     }
   },

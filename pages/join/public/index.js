8f6767f65c42a02ca854b6abace556a5ceee6d6c
takashi0602 <takashioicsn@gmail.com>
2020-10-31 03:10:45 +0900

    add: error message. modify: table style, typo

--- pages/join/public/index.js
+++ pages/join/public/index.js
@@ -7,15 +7,40 @@ export default {
   data() {
     return {
       worlds: [
-        { Id: 'ABCDEF', role: 'ばいきんくん' },
-        { Id: 'ABCDEF', role: 'ばいきんくん' },
-        { Id: 'ABCDEF', role: 'ばいきんくん' },
-        { Id: 'ABCDEF', role: 'ばいきんくん' },
-        { Id: 'ABCDEF', role: 'ばいきんくん' },
-        { Id: 'ABCDEF', role: 'ばいきんくん' },
-        { Id: 'ABCDEF', role: 'ばいきんくん' },
-        { Id: 'ABCDEF', role: 'ばいきんくん' },
+        { id: 'AAAAAA', role: 1 },
+        { id: 'BBBBBB', role: 2 },
+        { id: 'CCCCCC', role: 1 },
+        { id: 'DDDDDD', role: 2 },
+        { id: 'EEEEEE', role: 1 },
+        { id: 'FFFFFF', role: 2 },
+        { id: 'GGGGGG', role: 1 },
+        { id: 'HHHHHH', role: 2 },
       ],
+      error: false,
+      errorMessage: 'さんかにしっぱいしました',
     }
+  },
+  methods: {
+    joinWorld(worldId) {
+      this.error = false
+      this.$checkWorldId({ worldId })
+        .then((res) => {
+          if (res.validity) {
+            sessionStorage.worldId = worldId
+            sessionStorage.token = res.token
+            sessionStorage.role = res.role
+            this.$router.push('/gohome')
+          } else {
+            this.error = true
+            this.errorMessage = 'さんかにしっぱいしました'
+          }
+        })
+        .catch((e) => {
+          this.error = true
+          this.errorMessage = e.data.msg
+            ? e.data.msg
+            : 'エラーがはっせいしました'
+        })
+    },
   },
 }

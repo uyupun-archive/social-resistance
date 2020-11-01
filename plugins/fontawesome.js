180d913ca1ad4630d7eb334c72c35324922476a3
suzuking1125 <b7165@oic.jp>
2020-11-01 10:15:55 +0900

    replace left and right to fortawesome,adjust tab's css

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { config, library } from '@fortawesome/fontawesome-svg-core'
-import { faClipboard, faCaretRight } from '@fortawesome/free-solid-svg-icons'
+import {
+  faClipboard,
+  faCaretRight,
+  faCaretLeft,
+} from '@fortawesome/free-solid-svg-icons'
 import { faCheckCircle } from '@fortawesome/free-regular-svg-icons'
 // nuxt.config.jsでCSSファイルを追加
 config.autoAddCss = false
 
 // 利用するアイコンを配列に追加
-const solidIcons = [faClipboard, faCaretRight, faCheckCircle]
+const solidIcons = [faClipboard, faCheckCircle, faCaretLeft, faCaretRight]
 
 // 利用するアイコンをlibraryに追加
 solidIcons.forEach((icon) => library.add(icon))

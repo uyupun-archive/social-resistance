043ccd7958e0cdb92c484efc3bb26d2aa9d70b0c
suzuking1125 <b7165@oic.jp>
2020-11-02 18:16:09 +0900

    add character dami data in JS and tab's function

--- pages/charactor-select/index.js
+++ pages/charactor-select/index.js
@@ -6,12 +6,22 @@ export default {
   },
   data() {
     return {
+      selectTab: Boolean(true),
+      charactersUsa: ['チンピラウサギ', 'ノーマルウサギ'],
+      charactersBaikin: ['バイキンくん', '双子ばいきん'],
       characterselected: false,
     }
   },
   methods: {
     isSelectCharacter() {
       this.characterselected = true
+    },
+    isSelectTab(value) {
+      if (value === true) {
+        console.log(this.charactersUsa[0])
+      } else {
+        console.log(this.charactersBaikin[0])
+      }
     },
   },
 }

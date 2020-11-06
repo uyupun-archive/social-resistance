fdbc957bf9015c0551273c270dbf22061e7b4e96
murakami1618 <b7350@oic.jp>
2020-11-06 16:25:26 +0900

    icon update character rename

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -7,13 +7,24 @@ import {
   faCaretLeft,
   faSync,
 } from '@fortawesome/free-solid-svg-icons'
-import { faCheckCircle } from '@fortawesome/free-regular-svg-icons'
+
+import {
+  faCheckCircle,
+  faQuestionCircle,
+} from '@fortawesome/free-regular-svg-icons'
 
 // nuxt.config.jsでCSSファイルを追加
 config.autoAddCss = false
 
 // 利用するアイコンを配列に追加
-const icons = [faClipboard, faCaretRight, faCaretLeft, faSync, faCheckCircle]
+const icons = [
+  faClipboard,
+  faCaretRight,
+  faCaretLeft,
+  faSync,
+  faCheckCircle,
+  faQuestionCircle,
+]
 
 // 利用するアイコンをlibraryに追加
 icons.forEach((icon) => library.add(icon))

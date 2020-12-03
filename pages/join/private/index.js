05496cbdc7c7ec4ea0c8bc4d2eacbb0b85191fcf
takashi0602 <takashioicsn@gmail.com>
2020-12-04 01:32:51 +0900

    refactor(join private): refactor jion private page style
    re #219

--- pages/join/private/index.js
+++ pages/join/private/index.js
@@ -29,7 +29,7 @@ export default {
           this.errorMessage = e.data.msg
         }
       )
-      if (res.validity) {
+      if (res?.validity) {
         const payload = {
           id: e.target.worldId.value,
           token: res.token,

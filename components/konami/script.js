b7234c98b81a8fa9cb6c1c07de703b1c72780725
Kazuki Tobita <kazukiti201@gmail.com>
2020-12-29 05:06:56 +0900

    feat: add gaming background
    #212

--- components/konami/script.js
+++ components/konami/script.js
@@ -10,31 +10,19 @@ export default {
   methods: {
     konamiCommand() {
       document.addEventListener('keydown', (event) => {
-        if (event.key === 'ArrowUp') {
-          this.keyInput += '上'
-        } else if (event.key === 'ArrowDown') {
-          this.keyInput += '下'
-        } else if (event.key === 'ArrowLeft') {
-          this.keyInput += '左'
-        } else if (event.key === 'ArrowRight') {
-          this.keyInput += '右'
-        } else if (event.key === 'A') {
-          this.keyInput += 'A'
-        } else if (event.key === 'B') {
-          this.keyInput += 'B'
-        } else if (event.key !== 'Shift') {
-          this.keyInput = ''
-        }
-        if (this.keyInput.length > 10) {
-          this.keyInput = this.commands.slice(1)
-        }
-        if (this.keyInput === '上上下下左右左右BA') {
-          this.changeBackgroundColor()
-        }
+        if (event.key === 'ArrowUp') this.keyInput += '上'
+        else if (event.key === 'ArrowDown') this.keyInput += '下'
+        else if (event.key === 'ArrowLeft') this.keyInput += '左'
+        else if (event.key === 'ArrowRight') this.keyInput += '右'
+        else if (event.key === 'A') this.keyInput += 'A'
+        else if (event.key === 'B') this.keyInput += 'B'
+        else if (event.key !== 'Shift') this.keyInput = ''
+        if (this.keyInput.length > 10) this.keyInput = this.keyInput.slice(1)
+        if (this.keyInput === '上上下下左右左右BA') this.changeBackgroundColor()
       })
     },
     changeBackgroundColor() {
-      document.getElementsByTagName('body')[0].style.backgroundColor = 'red'
+      document.getElementsByTagName('body')[0].classList.add('bg-gaming')
     },
   },
 }

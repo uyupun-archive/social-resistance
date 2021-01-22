232de3f85fa59f3c86b62325c8651c97131d839b
takashi0602 <takashioicsn@gmail.com>
2021-01-23 04:22:38 +0900

    feat: add: profile api to profile page
    re #299

--- pages/profile/_id/script.js
+++ pages/profile/_id/script.js
@@ -7,46 +7,22 @@ export default {
   layout: 'after-login/index',
   data() {
     return {
-      user: {
-        id: '',
-        icon: 0,
-        rate: 0,
-        history: '',
-      },
-      userIcons: [
-        {
-          name: 'チンピラウサギ_1',
-          url: 'http://placehold.it/200',
-        },
-        {
-          name: 'チンピラウサギ_2',
-          url: 'http://placehold.it/200/ff0000/ffffff',
-        },
-        {
-          name: 'チンピラウサギ_3',
-          url: 'http://placehold.it/200/00ff00/ffffff',
-        },
-        {
-          name: 'チンピラウサギ_4',
-          url: 'http://placehold.it/200/0000ff/ffffff',
-        },
-      ],
+      user: null,
+      userId: this.$store.state.auth.userId,
     }
   },
-  mounted() {
-    this.getUserInfo()
+  async mounted() {
+    this.user = await this.$fetchProfile({ userId: this.userId })
   },
   methods: {
-    getUserInfo() {
-      // TODO: ユーザー情報の取得
-      this.user.id = 'ABCDEF'
-      this.user.icon = 0
-      this.user.rate = 1200
-      this.user.history = '10勝5敗(50%)'
+    retAvatar() {
+      return `${process.env.API_URL + this.user.avatar}`
     },
-    getRateIcon() {
-      // TODO: レート数に応じたアイコン(画像)を返す
-      return 'http://placehold.it/100'
+    retRank() {
+      return `${process.env.API_URL + this.user.rank}`
+    },
+    retHistory() {
+      return `${this.user.history.win}勝${this.user.history.lose}負`
     },
   },
 }

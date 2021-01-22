73a8f737bf636072a3508b6329069fdcb784d4ff
takashi0602 <takashioicsn@gmail.com>
2021-01-23 02:45:20 +0900

    feat: add: ranking api to ranking page
    re #299

--- pages/ranking/script.js
+++ pages/ranking/script.js
@@ -2,15 +2,21 @@ export default {
   layout: 'after-login/index',
   data() {
     return {
-      rankings: [
-        { userId: 'AAAAAA', rate: '1800', rank: '1st' },
-        { userId: 'BBBBBB', rate: '1700', rank: '2nd' },
-        { userId: 'CCCCCC', rate: '1600', rank: '3rd' },
-        { userId: 'DDDDDD', rate: '1500', rank: '4th' },
-        { userId: 'EEEEEE', rate: '1400', rank: '5th' },
-        { userId: 'FFFFFF', rate: '1300', rank: '6th' },
-        { userId: 'GGGGGG', rate: '1200', rank: '7th' },
-      ],
+      rankings: [],
     }
+  },
+  async mounted() {
+    this.rankings = await this.$fetchRanking()
+  },
+  methods: {
+    retRank(num) {
+      if (num === 1) return `${num}st`
+      else if (num === 2) return `${num}nd`
+      else if (num === 3) return `${num}rd`
+      else return `${num}th`
+    },
+    retImage(path) {
+      return `${process.env.API_URL + path}`
+    },
   },
 }

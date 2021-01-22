c5bbfc0d6e90c1291b8bd83dc4d801a8d17ad680
takashi0602 <takashioicsn@gmail.com>
2021-01-23 01:54:25 +0900

    feat: add: ranks API to rank page
    re #299

--- pages/rank/script.js
+++ pages/rank/script.js
@@ -2,18 +2,10 @@ export default {
   layout: 'after-login/index',
   data() {
     return {
-      ranks: [
-        { name: 'Pandemic', rate: '1800～' },
-        { name: 'Cluster', rate: '1600～1799' },
-        { name: 'Aerosol', rate: '1400～1599' },
-        { name: 'Virus', rate: '1200～1399' },
-        { name: 'Vaccine', rate: '1000～1199' },
-        { name: 'Doctor', rate: '800～999' },
-        { name: 'Mask', rate: '600～799' },
-        { name: 'Resistance', rate: '400～599' },
-        { name: 'Patient', rate: '200～399' },
-        { name: 'Zombie', rate: '0～199' },
-      ],
+      ranks: [],
     }
+  },
+  async mounted() {
+    this.ranks = (await this.$fetchRanks()).reverse()
   },
 }

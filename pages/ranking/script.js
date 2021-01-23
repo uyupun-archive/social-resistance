0675295c19525a7dc3eb55f96b93d29edfcdf676
takashi0602 <takashioicsn@gmail.com>
2021-01-24 03:35:45 +0900

    refactor: rename: method name
    re #299

--- pages/ranking/script.js
+++ pages/ranking/script.js
@@ -13,13 +13,17 @@ export default {
     if (res) this.rankings = res
   },
   methods: {
-    retRank(num) {
-      if (num === 1) return `${num}st`
-      else if (num === 2) return `${num}nd`
-      else if (num === 3) return `${num}rd`
+    makeOrdinalRank(num) {
+      const remainder = {
+        10: num % 10,
+        100: num % 100,
+      }
+      if (remainder[10] === 1 && remainder[100] !== 11) return `${num}st`
+      else if (remainder[10] === 2 && remainder[100] !== 12) return `${num}nd`
+      else if (remainder[10] === 3 && remainder[100] !== 13) return `${num}rd`
       else return `${num}th`
     },
-    retImage(path) {
+    makeFullImagePath(path) {
       return `${process.env.API_URL + path}`
     },
   },

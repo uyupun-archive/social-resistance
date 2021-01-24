b17145650a60ccdbf25b3d5271877c591731933d
takashi0602 <takashioicsn@gmail.com>
2021-01-25 01:37:06 +0900

    refactor: add: fetchRanks method
    re #299

--- pages/rank/script.js
+++ pages/rank/script.js
@@ -6,15 +6,18 @@ export default {
       errorMsg: '',
     }
   },
-  async mounted() {
-    const res = await this.$fetchRanks().catch((e) => {
-      this.errorMsg = 'ランクのしゅとくにしっぱいしました'
-    })
-    if (res) this.ranks = res.reverse()
+  mounted() {
+    this.fetchRanks()
   },
   methods: {
-    makeFullImagePath(path) {
-      return `${process.env.API_URL + path}`
+    async fetchRanks() {
+      const res = await this.$fetchRanks().catch(() => {
+        this.errorMsg = 'ランクのしゅとくにしっぱいしました'
+      })
+      if (res) this.ranks = res.reverse()
+    },
+    getFullImagePath(path) {
+      return process.env.API_URL + path
     },
   },
 }
